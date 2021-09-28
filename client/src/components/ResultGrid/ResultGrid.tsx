647f4644d9c37552e7a5a5da76b4428f2d5a9240
Lee44 <jlee7772@yahoo.com>
2021-09-28 11:34:55 -0700

    Redux fetches and loads data correctly into ResultGrid

--- client/src/components/ResultGrid/ResultGrid.tsx
+++ client/src/components/ResultGrid/ResultGrid.tsx
@@ -1,22 +1,22 @@
 import { Container, Grid } from "@mui/material";
 import ResultGridItems from "./ResultGridItems/ResultGridItems";
+import { useAppSelector } from "../../redux/hooks";
 
 export const ResultGrid = () => {
+	const products = useAppSelector((state) => {
+		return state.products.result;
+	});
+
 	return (
 		<Container maxWidth="xl">
 			<Grid container spacing={1} sx={{ marginTop: 1, marginBottom: 1 }}>
-				<Grid item xs={12} md={6} lg={3}>
-					<ResultGridItems />
-				</Grid>
-				<Grid item xs={12} md={6} lg={3}>
-					<ResultGridItems />
-				</Grid>
-				<Grid item xs={12} md={6} lg={3}>
-					<ResultGridItems />
-				</Grid>
-				<Grid item xs={12} md={6} lg={3}>
-					<ResultGridItems />
-				</Grid>
+				{products.map((product) => {
+					return (
+						<Grid item xs={12} md={6} lg={3}>
+							<ResultGridItems {...product} />
+						</Grid>
+					);
+				})}
 			</Grid>
 		</Container>
 	);
--- client/src/components/ResultGrid/ResultGridItems/ResultGridItems.tsx
+++ client/src/components/ResultGrid/ResultGridItems/ResultGridItems.tsx
@@ -6,17 +6,18 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import StarRating from "../../StarRating/StarRating";
+import type { Product } from "../../../redux/api";
 
-const ResultGridItems = () => {
+const ResultGridItems = (props: Product) => {
 	return (
 		<Card>
-			<CardMedia component="img" alt="green iguana" image="" />
+			<CardMedia sx={{ height: 150, objectFit: "contain" }} component="img" alt="product_image" image={`data:image/jpeg;base64,${props.image}`} />
 			<CardContent sx={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "start" }}>
-				<StarRating />
+				<StarRating rating={props.stars} reviews={props.reviews} />
 				<Typography gutterBottom variant="h5">
-					AMD Ryzen 5800X
+					{props.name}
 				</Typography>
-				<Typography variant="h6">$399.99</Typography>
+				<Typography variant="h6">${props.price}</Typography>
 			</CardContent>
 			<CardActions sx={{ justifyContent: "center" }}>
 				<Button variant="contained" size="large" startIcon={<ShoppingCartIcon />}>
