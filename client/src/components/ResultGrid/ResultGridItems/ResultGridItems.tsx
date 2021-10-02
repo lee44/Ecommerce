02bf21511ca0d715debbe7676d4ffeafda73c34f
Lee44 <jlee7772@yahoo.com>
2021-10-02 15:25:34 -0700

    Finished data import for processor and model

--- client/src/components/ResultGrid/ResultGridItems/ResultGridItems.tsx
+++ client/src/components/ResultGrid/ResultGridItems/ResultGridItems.tsx
@@ -1,13 +1,13 @@
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import StarRating from "../../StarRating/StarRating";
+import { useHistory, useLocation } from "react-router";
 import type { Product } from "../../../redux/api";
-import { useHistory, useLocation, useParams } from "react-router";
+import StarRating from "../../StarRating/StarRating";
 
 const ResultGridItems = (props: Product) => {
 	let history = useHistory();
@@ -21,7 +21,7 @@ const ResultGridItems = (props: Product) => {
 
 	return (
 		<Card>
-			<CardMedia sx={{ height: 150, objectFit: "contain" }} component="img" alt="product_image" image={`data:image/jpeg;base64,${props.image}`} />
+			<CardMedia sx={{ height: 150, objectFit: "contain" }} component="img" alt="product_image" image={`${props.image}`} />
 			<CardContent sx={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "start" }}>
 				<StarRating rating={props.stars} reviews={props.reviews} />
 				<Typography gutterBottom variant="h5">
