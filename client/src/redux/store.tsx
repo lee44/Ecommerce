f5a07acc5361b3f9547b309370206a7368850059
Lee44 <jlee7772@yahoo.com>
2021-10-08 23:08:10 -0700

    Added cartSlice with mui Badge

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
 import productsReducer from "./productSlice";
+
 export const store = configureStore({
 	reducer: {
 		products: productsReducer,
+		cart: cartReducer,
 	},
 });
 
